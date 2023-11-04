a562b55a042fd45b41ba38ee163b81275dbff5cd
SunnyNagave <88137080+SunnyNagave@users.noreply.github.com>
2023-11-04 11:57:48 +0530

    Update CalculatorBody.js

--- src/components/CalculatorBody/CalculatorBody.js
+++ src/components/CalculatorBody/CalculatorBody.js
@@ -8,12 +8,15 @@ export default function CalculatorBody(){
         <>
         <div className="row justify-content-center">
                 <div className="col-3 padbottom px-1">
+                    {/* on click clear action from reducer will be executed */}
                     <button onClick={()=> dispatch(actions.clear())} className="btn btn-outline-danger stdsize">AC</button>
                 </div>
                 <div class="col-3 padbottom px-1">
+                    {/* on click delete action from reducer will be executed */}
                     <button onClick={()=> dispatch(actions.delete())} className="btn btn-outline-danger stdsize">C</button>
                 </div> 
                 <div class="col-3 padbottom px-1">
+                    {/* on click addOperator action with payload - % will be executed and similarly for other operations */}
                     <button onClick={()=> dispatch(actions.addOperator("%"))} className="btn btn-outline-warning stdsize">%</button>
                 </div>
                 <div class="col-3 padbottom px-1">
@@ -22,6 +25,7 @@ export default function CalculatorBody(){
             </div>
             <div className="row justify-content-center">
                 <div className="col-3 padbottom px-1">
+                    {/* on click addNumber action with payload - 7 will be executed and similarly for other numbers */}
                     <button onClick={()=> dispatch(actions.addNumber("7"))} className="btn btn-outline-primary stdsize">7</button>
                 </div>
                 <div className="col-3 padbottom px-1">
@@ -70,6 +74,7 @@ export default function CalculatorBody(){
                     <button onClick={()=> dispatch(actions.addNumber("."))} className="btn btn-outline-primary stdsize">.</button>
                 </div>
                 <div className="col-3 px-1">
+                    {/* on click equals action from reducer will be executed */}
                     <button onClick={()=> dispatch(actions.equals())} className="btn btn-outline-success stdsize">=</button>
                 </div>
             </div>
