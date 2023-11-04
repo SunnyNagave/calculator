d0f8c68aea265d963e9e8a9352e75d0a89d7d2e6
SunnyNagave <88137080+SunnyNagave@users.noreply.github.com>
2023-11-04 11:41:04 +0530

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
 import { calculatorReducer } from "./reducers/calculatorReducer";
 import { configureStore } from "@reduxjs/toolkit";
 
+// Configuring redux store.
 export const store = configureStore({
     reducer:{
         calculatorReducer
